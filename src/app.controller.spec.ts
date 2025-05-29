b6ad758891ea90997d8f384a7515bf99bda75a7a
Felix Serang <byte007@zoho.com>
2025-05-29 20:16:11 +0700

    fix: index path caption

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "DevSecOps Trial Project"', () => {
+      expect(appController.getHello()).toBe('<h3>DevSecOps Trial Project</h3>');
     });
   });
 });
