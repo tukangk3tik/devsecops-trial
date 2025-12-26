f9444f39474301d749a3b0fe5ccd834c51f2d3e9
Felix Serang <byte007@zoho.com>
2025-12-26 19:46:52 +0700

    feat: test update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h3>DevSecOps Trial Project Updated</h3>';
+    return '<h3>DevSecOps Trial Project Updated Feat!</h3>';
   }
 }
